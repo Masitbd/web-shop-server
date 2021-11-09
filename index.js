eca358223a9f7a882a558bf65225a07199652d3b
Murad Sarker <sarker24@gmail.com>
2021-11-09 21:41:17 +0100

    Add middleware

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello Babs baby-shop server");
 });
+
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Listening at:${port}`);
